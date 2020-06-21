33f91727a0996a7d1063b3caae9240893d1114fc
Kaíque P. Santos <kaiquepsantos@protonmail.com>
2020-06-21 01:35:31 -0300

    Add files via upload

---
+++ exercicio_2.R
@@ -0,0 +1,319 @@
+install.packages( "magrittr" )
+install.packages( "tidyverse" )
+install.packages( "poliscidata" )
+
+library( magrittr )
+library( tidyverse )
+library( poliscidata )
+
+#
+# Suponha que tenhamos o dataframe df abaixo
+#
+# x     y
+# A     5
+# A     3
+# B     8
+# B    12
+#
+# Complete o código que obtém o seguinte resultado:
+#
+#        z
+#        7
+#
+
+df <- data.frame( c( "A", "A", "B", "B" ), c( 5, 3, 8, 12 ) )
+
+colnames( df ) <- c( "x", "y" )
+
+df %>%  summarise( z = mean( y ) )
+
+#######################################################################
+
+# Suponha que tenhamos o dataframe df abaixo
+#
+# y1    y2    y3    y4
+# 8.04  9.14  7.46  6.58
+# 6.95  8.14  6.77  5.76
+# 7.58  8.74  12.74 7.71
+#
+# Complete o código que obtém o seguinte resultado:
+#
+# y1    
+# 8.04  
+# 6.95  
+# 7.58  
+
+df <- data.frame( c( 8.04, 6.95, 7.58 ), 
+                  c( 9.14, 8.14, 8.74 ), 
+                  c( 7.46, 6.77, 12.74 ), 
+                  c( 6.58, 5.76, 7.71 ) )
+
+colnames( df ) <- c( "y1", "y2", "y3", "y4" )
+
+df %>% select( "y1" )
+
+  
+#######################################################################
+
+# Suponha que tenhamos o dataframe df abaixo
+#
+#    x  y
+#   1  10
+#   6  8
+#   2  3
+#   4  5
+#
+# Complete o código que obtém o seguinte resultado, fazendo uma operação
+# entre x e y
+#
+#    x  y   z
+#   1  10  -9
+#   6  8   -2
+#   2  3   -1
+#   4  5   -1
+#
+
+df <- data.frame( c( 1, 6, 2, 4 ), c( 10, 8, 3, 5 ) )
+
+colnames( df ) <- c( "x", "y " )
+
+df %>% mutate( z = df$x - df$y )
+
+########################################################################
+
+#
+# Suponha que tenhamos o dataframe df abaixo
+#
+#    city sales
+# Boston   220
+# Boston   125
+#    NYC   150
+#    NYC   250
+#
+# Complete o código que obtém o seguinte resultado:
+#
+# city   avg_sales
+# Boston      172
+# NYC         200 
+
+df <- data.frame( c( "Boston", "Boston", "NYC", "NYC" ),
+                  c( 220, 125, 150, 250 ) )
+
+colnames( df ) <- c( "city", "sales" )
+
+df %>% group_by( city ) %>%
+       summarise( avg_sales = mean( sales ) )
+  
+########################################################################
+
+# Suponha que tenhamos o dataframe df abaixo
+#
+#week   min   max
+#  3    55    60
+#  2    52    56
+#  1    60    63
+#  4    65    67
+#
+# Complete o código que obtém o seguinte resultado:
+#
+#week   min   max
+#  1    60    63
+#  2    52    56
+#  3    55    60
+#  4    65    67
+
+df <- data.frame( c( 3, 2, 1, 4 ),
+                  c( 55, 52, 60, 65 ),
+                  c( 60, 56, 63, 67 ) )
+
+colnames( df ) <- c( "week", "min", "max" )
+
+df %>% arrange( df )
+
+########################################################################
+
+# Suponha que tenhamos o dataframe df abaixo
+#
+# x_b_1  x_b_2  y_c_1  y_c_2
+#  A      2      W1     25
+#  A      4      W2     21
+#  B      6      W1     26
+#  B      8      W2     30
+#
+# Complete o código que obtém o seguinte resultado:
+#
+# y_c_1  y_c_2
+#  W1     25
+#  W2     21
+#  W1     26
+#  W2     30
+
+df <- data.frame( c( "A", "A", "B", "B" ),
+                  c( 2, 4, 6, 8 ),
+                  c( "W1", "W2", "W1", "W2" ),
+                  c( 25, 21, 26, 30 ) )
+
+colnames( df ) <- c( "x_b_1", "x_b_2", "y_c_1", "y_c_2")
+
+df %>% select( c( 3:4 ) )
+
+#########################################################################
+
+# Suponha que tenhamos o dataframe df abaixo
+#
+#     Sepal.Length Sepal.Width Petal.Length Petal.Width    Species
+# 78           6.7         3.0          5.0         1.7 versicolor
+# 121          6.9         3.2          5.7         2.3  virginica
+# 11           5.4         3.7          1.5         0.2     setosa
+# 92           6.1         3.0          4.6         1.4 versicolor
+# 146          6.7         3.0          5.2         2.3  virginica
+# 62           5.9         3.0          4.2         1.5 versicolor
+# 50           5.0         3.3          1.4         0.2     setosa
+# 17           5.4         3.9          1.3         0.4     setosa
+# 69           6.2         2.2          4.5         1.5 versicolor
+# 143          5.8         2.7          5.1         1.9  virginica
+#
+# Complete o código que obtém o seguinte resultado:
+#
+#Species      Sepal.Area
+#versicolor      20.10
+#virginica       22.08
+#setosa          19.98
+#versicolor      18.30
+#virginica       20.10
+#versicolor      17.70
+#setosa          16.50
+#setosa          21.06
+#versicolor      13.64
+#virginica      15.66
+
+df <- iris[ c( 78, 121, 11, 92, 146, 62, 50, 17, 69, 143 ), ]
+
+df %>% mutate( Sepal.Area = Sepal.Length * Sepal.Width )
+
+########################################################################
+
+# Suponha que tenhamos o dataframe df abaixo
+#
+#name         start       end         party     
+#Eisenhower   1953-01-20  1961-01-20  Republican
+#Kennedy      1961-01-20  1963-11-22  Democratic
+#Johnson      1963-11-22  1969-01-20  Democratic
+#Nixon        1969-01-20  1974-08-09  Republican
+#Ford         1974-08-09  1977-01-20  Republican
+#Carter       1977-01-20  1981-01-20  Democratic
+#Reagan       1981-01-20  1989-01-20  Republican
+#Bush         1989-01-20  1993-01-20  Republican
+#Clinton      1993-01-20  2001-01-20  Democratic
+#Bush         2001-01-20  2009-01-20  Republican
+#Obama        2009-01-20  2017-01-20  Democratic
+#
+#Crie um código abaixo para que se altere a variável party
+#deixando apenas a primeira letra dos partidos
+
+df <- data.frame( c( "Eisenhower", "Kennedy", "Johnson", "Nixon", "Ford", "Carter", "Reagan", "Bush", "Clinton", "Bush", "Obama" ),
+                  c( "1953-01-20", "1961-01-20", "1963-11-22", "1969-01-20", "1974-08-09",
+                     "1977-01-20", "1981-01-20", "1989-01-20", "1993-01-20", "2001-01-20", "2009-01-20" ),
+                  c( "1961-01-20", "1963-11-22", "1969-01-20", "1974-08-09", "1977-01-20",
+                     "1981-01-20", "1989-01-20", "1993-01-20", "2001-01-20", "2009-01-20", "2017-01-20"),
+                  c( "Republican", "Democratic", "Democratic", "Republican", "Republican", "Democratic",
+                     "Republican", "Republican", "Democratic", "Republican", "Democratic") )
+
+colnames( df ) <- c( "name", "start", "end", "party" )
+
+df %>% mutate( party = case_when( party == "Republican" ~ "R",
+                                  party == "Democratic" ~ "D" ) )
+
+###############################################################################
+
+# No pacote poliscidata existe um banco de dados chamado nes, com informações 
+# do American National Election Survey. Para os exerícicios a seguir, instale 
+# o pacote poliscidata e tidyverse, carregue-os e crie um objeto chamado
+# df com os dados do nes. 
+
+df <- poliscidata::nes
+
+# Faça uma primeira exploração do banco de dados com todos os comandos
+# passados até aqui que possuem esse objetivo
+
+summary( df )
+
+glimpse( df )
+
+# Quantos respondentes possui na pesquisa?
+
+count( df )
+
+# RESPOSTA: 5916 respondentes
+
+# Caso queiram ter mais informações sobre as variáveis do nes, basta rodar
+# o código `?nes`, que no canto inferior direito aparecerá uma descrição.
+# Como temos muitas variáveis, deixe apenas as colunas
+# ftgr_cons, dem_raceeth, voted2012, science_use, preknow3, obama_vote
+# income5, gender.
+
+?nes
+
+df_selec <- df %>% select( ftgr_cons, dem_raceeth, science_use, preknow3, obama_vote, income5, gender )
+
+# Se quisermos ter informações apenas de pessoas que votaram na
+# eleição de 2012, podemos usar a variável voted2012. Tire do banco
+# os respondentes que não votaram
+
+df_filtro <- df %>% filter( voted2012 == "Voted" )
+
+# Quantos respondentes sobraram?
+
+count( df_filtro )
+
+# Crie uma variável chamada white que indica se o respondente é branco
+# ou não a partir da variável dem_raceeth, crie a variável ideology a
+# partir da variável ftgr_cons (0-33 como liberal, 34 a 66 como centro,
+# e 67 a 100 como conservador), ao mesmo tempo em que muda
+# a variável obama_vote para trocar o 1 por "Sim" e 0 por "não"
+
+# COMENTÁRIO: Eu não sei se é para criar variáveis isoladas ou data frames com as variáveis indicadas. Farei os dois
+
+# Criando variáveis isoladas
+white <- df %>% transmute( cor = case_when( dem_raceeth == "1. White non-Hispanic" ~ "BRANCO",
+                                            dem_raceeth != "1. White non-Hispanic" ~ "NAO_BRANCO" ) )
+
+ideology <- df %>% transmute( ideologia = case_when( ftgr_cons < 34 ~ "Liberal",
+                                                  ftgr_cons >= 34 && ftgr_cons <= 66 ~ "Centro",
+                                                  ftgr_cons >= 67 ~ "Conservador" ) )
+
+# Criando data frames com as variáveis
+
+
+
+df_obama <- df %>% mutate( white = case_when( dem_raceeth == "1. White non-Hispanic" ~ "BRANCO",
+                                              dem_raceeth != "1. White non-Hispanic" ~ "NAO_BRANCO" ) ) %>%
+                   mutate( ideology = case_when( ftgr_cons < 34 ~ "Liberal",
+                                                 ftgr_cons >= 34 && ftgr_cons <= 66 ~ "Centro",
+                                                 ftgr_cons >= 67 ~ "Conservador" ) ) %>%
+                   transmute( obama_vote = case_when( obama_vote == 0 ~ "Nao",
+                                                   obama_vote == 1 ~ "Sim" ) )
+
+glimpse( df_obama$obama_vote )
+
+# Demonstre como observar a quantidade de pessoas em cada uma das
+# categorias de science_use
+
+df %>% count( science_use )
+
+# Demonstre como observar a média de conservadorismo (variável 
+# ftgr_cons) para cada categoria de science_use
+
+df_novo_filtro <- df_filtro
+
+df_novo_filtro$ftgr_cons <- as.numeric( df_filtro$ftgr_cons )
+
+df_novo_filtro %>% group_by( science_use ) %>%
+                   summarise( mean( ftgr_cons, na.rm = TRUE ) )
+   
+###############################################################################
+
+# Responder as questões teóricas da aula abaixo
+
+
+
